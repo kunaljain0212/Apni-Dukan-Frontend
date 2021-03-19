cdaca222cc4116c9a7c4884e5003853e7d86b863
Kunal Jain <jaikunal209@gmail.com>
2021-03-20 00:31:49 +0530

    Adding support for docker setup

--- src/backend.js
+++ src/backend.js
@@ -1,2 +1,5 @@
-export const API = process.env.REACT_APP_BACKEND;
+export const API =
+  process.env.REACT_APP_ENVIRONMENT === "localhost"
+    ? process.env.REACT_APP_BACKEND_LOCAL
+    : process.env.REACT_APP_BACKEND;
 export const KEY_ID = process.env.REACT_APP_KEY_ID;
